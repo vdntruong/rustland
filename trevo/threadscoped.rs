c9fc5409e4203bcdf4978d703dd3e49806352ab4
vdntruong <truongvodainhat@gmail.com>
2026-01-04 09:00:07 +0700

    Update scoped thread

--- trevo/threadscoped.rs
+++ trevo/threadscoped.rs
@@ -1,5 +1,9 @@
 /*
 
+Scope thread is a way to create a new thread and join it to the main thread.
+
+The primary benefit of scope is that it allows threads to safely borrow non-static data from the parent scope without needing to use synchronization primitives like Arc or Mutex.
+
 https://doc.rust-lang.org/book/ch13-01-closures.html
 https://doc.rust-lang.org/std/thread/fn.scope.html
  */
@@ -21,17 +25,27 @@ pub fn test() {
     //     println!("My age is {age}");
     //     println!("This is person name {}", pete.name);
     // };
-    let print_age = move || {
-        println!("This is the child closure");
-        println!("My age is {age}");
-        println!("This is person name {}", &pete.name);
-    };
 
     // with '_' prefix, we are not interested in the result of the thread
     // and the compiler will not warn us if we don't use the result
-    let _result = thread::spawn(print_age).join();
-    println!("Finished printing age");
+    // let _result = thread::spawn(print_age).join();
+    // println!("Finished printing age");
 
-    // println!("My age is {age}");                     // we can print age because closure just copied the value
-    // println!("This is person name {}", pete.name);   // but we can not print name because we moved ownership to the thread
+    // scope
+
+    let print_age2 = || {
+        // removed 'move' keyword
+        println!("This is the child closure");
+        println!("My age is {age}");
+        println!("This is person name {}", &pete.name); // using reference to avoid moving ownership (intead of using 'move' to move ownership)
+    };
+    thread::scope(|s| {
+        // we will call scope's spawn method
+        s.spawn(print_age2); // scope auto join threads to the main thread
+        s.spawn(print_age2);
+    });
+
+    println!("This is the main thread");
+    println!("My age is {age}"); // we can print age because closure just copied the value
+    println!("This is person name {}", pete.name); // but we can not print name because we moved ownership to the thread
 }
